3d081cfc67252ab3df23d6b9d5c7901060e3c665
cmkroh <cmkroh@gmail.com>
2025-06-20 22:42:50 +0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+function Header({ onAboutClick, onContactClick }) {
+  return (
+    <header className="site-header">
+      <h1>Your Name</h1>
+      <nav>
+        <button onClick={onAboutClick}>About</button>
+        <button onClick={onContactClick}>Contact</button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
